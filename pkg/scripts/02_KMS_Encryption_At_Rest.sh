8c93213e44ae5c8fc6b155e9373aae1187573f35
kaijun123 <kaijun123@gmail.com>
2023-07-18 18:43:25 +0800

    Shifted yaml files into script directory

--- pkg/scripts/02_KMS_Encryption_At_Rest.sh
+++ pkg/scripts/02_KMS_Encryption_At_Rest.sh
@@ -4,8 +4,8 @@ cd /etc/kubernetes
 
 sudo mkdir encryption
 
-sudo cp /mount/pkg/plugins/plugin.yaml /etc/kubernetes/manifests/plugin.yaml
+sudo cp /mount/pkg/scripts/plugin.yaml /etc/kubernetes/manifests/plugin.yaml
 
-sudo cp /mount/pkg/plugins/kms-encryption-conf.yaml /etc/kubernetes/encryption/encryption-conf.yaml
+sudo cp /mount/pkg/scripts/kms-encryption-conf.yaml /etc/kubernetes/encryption/encryption-conf.yaml
 
-sudo cp /mount/pkg/plugins/kms-kube-apiserver.yaml /etc/kubernetes/manifests/kube-apiserver.yaml
+sudo cp /mount/pkg/scripts/kms-kube-apiserver.yaml /etc/kubernetes/manifests/kube-apiserver.yaml
